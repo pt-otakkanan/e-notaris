4a81a16adab6a2467dacf8e4b97fe6e9ec771c75
yasmnask <zakiyahyasmin1@gmail.com>
2025-08-20 08:44:44 +0700

    feat: ui welcome page

--- src/features/deeds/components/DeedAddRequirementModalBody.js
+++ src/features/deeds/components/DeedAddRequirementModalBody.js
@@ -11,7 +11,18 @@ export default function DeedAddRequirementModalBody({
       <div>
         <InputText
           type="Nama Dokumen"
-          labelTitle="Nama"
+          labelTitle="Nama Dokumen"
+          labelStyle="after:content-['*'] after:ml-1 after:text-red-500"
+          defaultValue=""
+          updateFormValue=""
+          updateType="text"
+        />
+      </div>
+
+      <div>
+        <InputText
+          type="Jenis Dokumen"
+          labelTitle="Jenis Dokumen"
           labelStyle="after:content-['*'] after:ml-1 after:text-red-500"
           defaultValue=""
           updateFormValue=""
@@ -22,6 +33,9 @@ export default function DeedAddRequirementModalBody({
       <div className="flex justify-end pt-2">
         <button className="btn" onClick={closeModal}>
           Tutup
+        </button>
+        <button className="btn btn-primary ms-2" onClick={closeModal}>
+          Simpan
         </button>
       </div>
     </div>
---
+++ src/features/user/components/LandingPoints.js
@@ -0,0 +1,17 @@
+function LandingPoints() {
+  return (
+    <>
+      {/* Text */}
+      <div className="text-center md:text-left">
+        <h1 className="text-3xl font-bold text-[#553c3e] text-center dark:text-[#efe0e1] mb-1">
+          Selamat Datang di E-Notaris!
+        </h1>
+        <p className="text-[#553c3e] text-center mt-0 dark:text-[#efe0e1]">
+          Membangun Pasti di Atas Janji
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default LandingPoints;
